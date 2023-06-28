b3d525568869cd89a34f059b07005d92a52d3cbe
AndreasIsl <92983953+AndreasIsl@users.noreply.github.com>
2023-06-28 14:08:05 +0200

    Added some changes for Implementation
    yes this is a short summary i added code your welcome

--- app/src/main/java/com/example/appdevkalender/MainActivity.kt
+++ app/src/main/java/com/example/appdevkalender/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.appdevkalender
 
+import android.annotation.SuppressLint
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.content.ContextCompat
@@ -20,13 +22,10 @@ class MainActivity : AppCompatActivity() {
     private  val year = calendar.get(Calendar.YEAR)
     private val month = calendar.get(Calendar.MONTH)
     private val day = calendar.get(Calendar.DAY_OF_MONTH)
+    private lateinit var calenderDayBtns: Array<Button>
 
-    private fun checkIfCurrentDate(date: Date): Boolean {
-        if (date.year == year && date.month == month) {
-            return true
-        }
-        return false
-    }
+
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -46,7 +45,10 @@ class MainActivity : AppCompatActivity() {
         val dateBtn = binding.btnDate
         dateBtn.text = "${months[chosenMonth]} $chosenYear"
 
-        val calenderDayBtns = arrayOf<Button>(
+        val statusTV = binding.statusTV
+        val appointmentTV = binding.appointmentsTV
+
+          calenderDayBtns = arrayOf<Button>(
             binding.btnKalender1,
             binding.btnKalender2,
             binding.btnKalender3,
@@ -80,8 +82,10 @@ class MainActivity : AppCompatActivity() {
             binding.btnKalender31
         )
 
-        calenderDayBtns[chosenDay - 1].setBackgroundColor(ContextCompat.getColor(this, R.color.green))
-        Log.d("test", "chosenMonth = ${months[0]}")
+
+        makeBtnInvisibleAndVisible(chosenMonth)
+        calenderDayBtns[chosenDay - 1].setBackgroundColor(R.color.green)
+
         previousBtn.setOnClickListener {
             if(chosenMonth  == 0){
                 chosenMonth = 11
@@ -91,6 +95,9 @@ class MainActivity : AppCompatActivity() {
                 chosenMonth --
                 dateBtn.text = "${months[chosenMonth]} ${chosenYear}"
             }
+            makeBtnInvisibleAndVisible(chosenMonth)
+            //check if its the current month if not make the currentdayBtn white
+
         }
 
         nextBtn.setOnClickListener {
@@ -103,6 +110,51 @@ class MainActivity : AppCompatActivity() {
                 dateBtn.text = "${months[chosenMonth]} ${chosenYear}"
 
             }
+            makeBtnInvisibleAndVisible(chosenMonth)
+            //check if its the current month if not make the currentdayBtn white
+
+        }
+
+
+        for (btn in calenderDayBtns.indices) {
+            calenderDayBtns[btn].setOnClickListener {
+                statusTV.text = "${btn + 1}.${chosenMonth + 1}.${chosenYear}"
+                appointmentTV.text = "${btn + 1} of ${months[chosenMonth]} no Appointments "
+            }
+        }
+
+
+
+
+    }
+
+    private fun checkIfCurrentDate(date: Date): Boolean {
+        if (date.year == year && date.month == month) {
+            return true
+        }
+        return false
+    }
+
+    private fun checkIfCurrentMonthIs31(month: Int): Boolean {
+        if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11){
+            return true
+        }
+        return false
+    }
+
+    private fun makeBtnInvisibleAndVisible(month: Int) {
+        if (checkIfCurrentMonthIs31(month)) {
+            calenderDayBtns[30].visibility = View.VISIBLE
+            calenderDayBtns[29].visibility = View.VISIBLE
+            calenderDayBtns[28].visibility = View.VISIBLE
+        } else if (month == 1) {
+            calenderDayBtns[30].visibility = View.GONE
+            calenderDayBtns[29].visibility = View.GONE
+            calenderDayBtns[28].visibility = View.GONE
+        } else {
+            calenderDayBtns[30].visibility = View.GONE
+            calenderDayBtns[29].visibility = View.VISIBLE
+            calenderDayBtns[28].visibility = View.VISIBLE
         }
     }
 }
